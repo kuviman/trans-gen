5b8d092fc06fadf0a7f4b6e4b52d9f498f98cdd6
kuviman <kuviman@gmail.com>
2021-01-12 17:00:55 +0400

    Initial commit

---
+++ generated-code/cpp/model/Model.hpp
@@ -0,0 +1,14 @@
+#ifndef _MODEL_HPP_
+#define _MODEL_HPP_
+
+#include "EntityType.hpp"
+#include "BuildProperties.hpp"
+#include "AttackProperties.hpp"
+#include "RepairProperties.hpp"
+#include "EntityProperties.hpp"
+#include "Player.hpp"
+#include "Vec2Int.hpp"
+#include "Entity.hpp"
+#include "PlayerView.hpp"
+
+#endif
---
+++ generated-code/rust/src/lib.rs
@@ -0,0 +1,27 @@
+
+mod entity_type;
+pub use self::entity_type::*;
+
+mod build_properties;
+pub use self::build_properties::*;
+
+mod attack_properties;
+pub use self::attack_properties::*;
+
+mod repair_properties;
+pub use self::repair_properties::*;
+
+mod entity_properties;
+pub use self::entity_properties::*;
+
+mod player;
+pub use self::player::*;
+
+mod vec2_i32;
+pub use self::vec2_i32::*;
+
+mod entity;
+pub use self::entity::*;
+
+mod player_view;
+pub use self::player_view::*;
