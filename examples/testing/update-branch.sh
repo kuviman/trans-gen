f7443b79f17437f949bb27445b6588d40604c859
kuviman <kuviman@gmail.com>
2021-01-16 03:14:20 +0400

    Remove unnecessary semicolon

--- examples/testing/update-branch.sh
+++ examples/testing/update-branch.sh
@@ -6,7 +6,7 @@ git worktree add branch testing
 cargo run --example testing -- generate branch
 pushd branch
 git add .
-git diff --cached --exit-code;
+git diff --cached --exit-code
 if [ $? -neq 0 ]; then
     git commit -m "Update generated code"
 fi
