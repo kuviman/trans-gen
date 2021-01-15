43014a164bf4c9b85d9beb0b77db2e01225bb851
kuviman <kuviman@gmail.com>
2021-01-16 03:19:52 +0400

    Fix CI exiting on git diff

--- examples/testing/update-branch.sh
+++ examples/testing/update-branch.sh
@@ -6,10 +6,7 @@ git worktree add branch testing
 cargo run --example testing -- generate branch
 pushd branch
 git add .
-git diff --cached --exit-code
-if [ $? -neq 0 ]; then
-    git commit -m "Update generated code"
-fi
+git diff --cached --exit-code || git commit -m "Update generated code"
 popd
 rm -rf branch
 git worktree prune
