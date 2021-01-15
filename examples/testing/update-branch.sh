6cea95be880d301525c68fd643c24344184a8eba
kuviman <kuviman@gmail.com>
2021-01-16 03:53:06 +0400

    Return generated-code subdir (or .git is removed)

--- examples/testing/update-branch.sh
+++ examples/testing/update-branch.sh
@@ -3,7 +3,7 @@ set -ex
 cd "$(dirname "$0")"
 
 git worktree add branch testing
-cargo run --example testing -- generate branch
+cargo run --example testing -- generate branch/generated-code
 pushd branch
 git add .
 git diff --cached --exit-code || git commit -m "Update generated code"
