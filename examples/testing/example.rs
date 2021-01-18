87e5616f658325100cd32fe51054cbcb29fdc017
kuviman <kuviman@gmail.com>
2021-01-18 09:29:23 +0400

    Documentation of example model

--- examples/testing/example.rs
+++ examples/testing/example.rs
@@ -6,7 +6,11 @@ use trans::Trans;
 #[trans_doc = "ru:Пример enum"]
 #[derive(Debug, Serialize, Deserialize, PartialEq, Eq, Hash, Copy, Clone, Trans)]
 pub enum Enumeration {
+    /// First option
+    #[trans_doc = "ru:Первый вариант"]
     ValueOne,
+    /// Second option
+    #[trans_doc = "ru:Второй вариант"]
     ValueTwo,
 }
 
@@ -17,19 +21,46 @@ pub struct NewTypeI32(i32);
 #[trans_doc = "ru:Пример one of"]
 #[derive(PartialEq, Debug, Serialize, Deserialize, Trans)]
 pub enum OneOf {
-    OptionOne { vec_i32: Vec<i32>, long_int: i64 },
-    OptionTwo { value: usize },
+    /// First option
+    #[trans_doc = "ru:Первый вариант"]
+    OptionOne {
+        /// List of integers
+        #[trans_doc = "ru:Список целых чисел"]
+        vec_i32: Vec<i32>,
+        /// Long integer
+        #[trans_doc = "ru:Длинное целое"]
+        long_int: i64,
+    },
+    /// Second option
+    #[trans_doc = "ru:Первый вариант"]
+    OptionTwo {
+        /// usize
+        #[trans_doc = "ru:usize"]
+        value: usize,
+    },
 }
 
 /// Example model
 #[trans_doc = "ru:Пример структуры"]
 #[derive(PartialEq, Debug, Serialize, Deserialize, Trans)]
 pub struct Structure {
+    /// First oneof
+    #[trans_doc = "ru:Первый oneof"]
     one_of_one: OneOf,
+    /// Second oneof
+    #[trans_doc = "ru:Второй oneof"]
     one_of_two: OneOf,
+    /// Dictionary
+    #[trans_doc = "ru:Таблица"]
     hash_map: HashMap<Enumeration, NewTypeI32>,
+    /// Text
+    #[trans_doc = "ru:Текст"]
     text: String,
+    /// 32-bit float
+    #[trans_doc = "ru:32-битное число с плавающей точкой"]
     float_number: f32,
+    /// 64-bit float
+    #[trans_doc = "ru:64-битное число с плавающей точкой"]
     double_number: f64,
 }
 
