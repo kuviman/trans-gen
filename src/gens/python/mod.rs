7825d1af42f52bccc3377a588dc9dd23a788840d
kuviman <kuviman@gmail.com>
2021-01-19 12:03:58 +0400

    Migrate Python to templing

--- src/gens/python/mod.rs
+++ src/gens/python/mod.rs
@@ -12,14 +12,10 @@ pub struct Generator {
     files: HashMap<String, String>,
 }
 
-fn write_struct(
-    writer: &mut Writer,
-    struc: &Struct,
-    base: Option<(&Name, usize)>,
-) -> std::fmt::Result {
-    // Imports
-    for field in &struc.fields {
-        fn add_imports(writer: &mut Writer, schema: &Schema) -> std::fmt::Result {
+fn imports(schema: &Schema) -> String {
+    let mut imports = BTreeSet::new();
+    fn add_imports_struct(struc: &Struct, imports: &mut BTreeSet<Name>) {
+        fn add_imports(schema: &Schema, imports: &mut BTreeSet<Name>) {
             match schema {
                 Schema::Struct(Struct { name, .. })
                 | Schema::OneOf {
@@ -28,22 +24,17 @@ fn write_struct(
                 | Schema::Enum {
                     base_name: name, ..
                 } => {
-                    writeln!(
-                        writer,
-                        "from .{} import {}",
-                        name.snake_case(conv),
-                        name.camel_case(conv)
-                    )?;
+                    imports.insert(name.clone());
                 }
                 Schema::Option(inner) => {
-                    add_imports(writer, inner)?;
+                    add_imports(inner, imports);
                 }
                 Schema::Vec(inner) => {
-                    add_imports(writer, inner)?;
+                    add_imports(inner, imports);
                 }
                 Schema::Map(key_type, value_type) => {
-                    add_imports(writer, key_type)?;
-                    add_imports(writer, value_type)?;
+                    add_imports(key_type, imports);
+                    add_imports(value_type, imports);
                 }
                 Schema::Bool
                 | Schema::Int32
@@ -52,224 +43,35 @@ fn write_struct(
                 | Schema::Float64
                 | Schema::String => {}
             }
-            Ok(())
         }
-        add_imports(writer, &field.schema)?;
+        for field in &struc.fields {
+            add_imports(&field.schema, imports);
+        }
     }
-
-    // Class
-    write!(writer, "class {}", struc.name.camel_case(conv))?;
-    if let Some((base_name, _)) = base {
-        write!(writer, "({})", base_name.camel_case(conv))?;
-    }
-    writeln!(writer, ":")?;
-    writer.inc_ident();
-    if let Some((_, tag)) = base {
-        writeln!(writer, "TAG = {}", tag)?;
-    }
-
-    // Constructor
-    write!(writer, "def __init__(self")?;
-    for field in &struc.fields {
-        write!(writer, ", {}", field.name.snake_case(conv))?;
-    }
-    writeln!(writer, "):")?;
-    for field in &struc.fields {
-        writeln!(
-            writer,
-            "    self.{} = {}",
-            field.name.snake_case(conv),
-            field.name.snake_case(conv)
-        )?;
-    }
-    if struc.fields.is_empty() {
-        writeln!(writer, "    pass")?;
-    }
-
-    // Reading
-    writeln!(writer, "@staticmethod")?;
-    writeln!(writer, "def read_from(stream):")?;
-    writer.inc_ident();
-    for field in &struc.fields {
-        fn assign(writer: &mut Writer, to: &str, schema: &Schema) -> std::fmt::Result {
-            match schema {
-                Schema::Bool => {
-                    writeln!(writer, "{} = stream.read_bool()", to)?;
-                }
-                Schema::Int32 => {
-                    writeln!(writer, "{} = stream.read_int()", to)?;
-                }
-                Schema::Int64 => {
-                    writeln!(writer, "{} = stream.read_long()", to)?;
-                }
-                Schema::Float32 => {
-                    writeln!(writer, "{} = stream.read_float()", to)?;
-                }
-                Schema::Float64 => {
-                    writeln!(writer, "{} = stream.read_double()", to)?;
-                }
-                Schema::String => {
-                    writeln!(writer, "{} = stream.read_string()", to)?;
-                }
-                Schema::Struct(Struct { name, .. })
-                | Schema::OneOf {
-                    base_name: name, ..
-                } => {
-                    writeln!(
-                        writer,
-                        "{} = {}.read_from(stream)",
-                        to,
-                        name.camel_case(conv)
-                    )?;
-                }
-                Schema::Option(inner) => {
-                    writeln!(writer, "if stream.read_bool():")?;
-                    writer.inc_ident();
-                    assign(writer, to, inner)?;
-                    writer.dec_ident();
-                    writeln!(writer, "else:")?;
-                    writeln!(writer, "    {} = None", to)?;
-                }
-                Schema::Vec(inner) => {
-                    writeln!(writer, "{} = []", to)?;
-                    writeln!(writer, "for _ in range(stream.read_int()):")?;
-                    writer.inc_ident();
-                    assign(writer, &format!("{}_element", to), inner)?;
-                    writeln!(writer, "{}.append({}_element)", to, to)?;
-                    writer.dec_ident();
-                }
-                Schema::Map(key_type, value_type) => {
-                    writeln!(writer, "{} = {{}}", to)?;
-                    writeln!(writer, "for _ in range(stream.read_int()):")?;
-                    writer.inc_ident();
-                    assign(writer, &format!("{}_key", to), key_type)?;
-                    assign(writer, &format!("{}_value", to), value_type)?;
-                    writeln!(writer, "{}[{}_key] = {}_value", to, to, to)?;
-                    writer.dec_ident();
-                }
-                Schema::Enum { base_name, .. } => {
-                    writeln!(
-                        writer,
-                        "{} = {}(stream.read_int())",
-                        to,
-                        base_name.camel_case(conv)
-                    )?;
-                }
+    match schema {
+        Schema::Struct(struc) => {
+            add_imports_struct(struc, &mut imports);
+        }
+        Schema::OneOf { variants, .. } => {
+            for variant in variants {
+                add_imports_struct(variant, &mut imports);
             }
-            Ok(())
         }
-        assign(writer, &field.name.snake_case(conv), &field.schema)?;
+        _ => {}
     }
-    write!(writer, "return {}(", struc.name.camel_case(conv))?;
-    let mut first = true;
-    for field in &struc.fields {
-        if first {
-            first = false;
-        } else {
-            write!(writer, ", ")?;
-        }
-        write!(writer, "{}", field.name.snake_case(conv))?;
-    }
-    writeln!(writer, ")")?;
-    writer.dec_ident();
+    include_templing!("src/gens/python/imports.templing")
+}
 
-    // Writing
-    writeln!(writer, "def write_to(self, stream):")?;
-    writer.inc_ident();
-    if base.is_some() {
-        writeln!(writer, "stream.write_int(self.TAG)")?;
-    } else if struc.fields.is_empty() {
-        writeln!(writer, "pass")?;
-    }
-    for field in &struc.fields {
-        fn write(writer: &mut Writer, value: &str, schema: &Schema) -> std::fmt::Result {
-            match schema {
-                Schema::Bool => {
-                    writeln!(writer, "stream.write_bool({})", value)?;
-                }
-                Schema::Int32 => {
-                    writeln!(writer, "stream.write_int({})", value)?;
-                }
-                Schema::Int64 => {
-                    writeln!(writer, "stream.write_long({})", value)?;
-                }
-                Schema::Float32 => {
-                    writeln!(writer, "stream.write_float({})", value)?;
-                }
-                Schema::Float64 => {
-                    writeln!(writer, "stream.write_double({})", value)?;
-                }
-                Schema::String => {
-                    writeln!(writer, "stream.write_string({})", value)?;
-                }
-                Schema::Struct(_) | Schema::OneOf { .. } => {
-                    writeln!(writer, "{}.write_to(stream)", value)?;
-                }
-                Schema::Option(inner) => {
-                    writeln!(writer, "if {} is None:", value)?;
-                    writeln!(writer, "    stream.write_bool(False)")?;
-                    writeln!(writer, "else:")?;
-                    writer.inc_ident();
-                    writeln!(writer, "stream.write_bool(True)")?;
-                    write(writer, value, inner)?;
-                    writer.dec_ident();
-                }
-                Schema::Vec(inner) => {
-                    writeln!(writer, "stream.write_int(len({}))", value)?;
-                    writeln!(writer, "for element in {}:", value)?;
-                    writer.inc_ident();
-                    write(writer, "element", inner)?;
-                    writer.dec_ident();
-                }
-                Schema::Map(key_type, value_type) => {
-                    writeln!(writer, "stream.write_int(len({}))", value)?;
-                    writeln!(writer, "for key, value in {}.items():", value)?;
-                    writer.inc_ident();
-                    write(writer, "key", key_type)?;
-                    write(writer, "value", value_type)?;
-                    writer.dec_ident();
-                }
-                Schema::Enum { .. } => {
-                    writeln!(writer, "stream.write_int({})", value)?;
-                }
-            }
-            Ok(())
-        }
-        write(
-            writer,
-            &format!("self.{}", field.name.snake_case(conv)),
-            &field.schema,
-        )?;
-    }
-    writer.dec_ident();
+fn read_var(var: &str, schema: &Schema) -> String {
+    include_templing!("src/gens/python/read_var.templing")
+}
 
-    // Repr
-    writeln!(writer, "def __repr__(self):")?;
-    writer.inc_ident();
-    writeln!(writer, "return \"{}(\" + \\", struc.name.camel_case(conv))?;
-    for (index, field) in struc.fields.iter().enumerate() {
-        write!(writer, "    repr(self.{})", field.name.snake_case(conv))?;
-        if index + 1 < struc.fields.len() {
-            write!(writer, " + \",\"")?;
-        }
-        writeln!(writer, " + \\")?;
-    }
-    writeln!(writer, "    \")\"")?;
-    writer.dec_ident();
+fn write_var(var: &str, schema: &Schema) -> String {
+    include_templing!("src/gens/python/write_var.templing")
+}
 
-    writer.dec_ident();
-
-    if let Some((base_name, _)) = base {
-        writeln!(
-            writer,
-            "{}.{} = {}",
-            base_name.camel_case(conv),
-            struc.name.camel_case(conv),
-            struc.name.camel_case(conv)
-        )?;
-    }
-
-    Ok(())
+fn struct_impl(struc: &Struct, base: Option<(&Name, usize)>) -> String {
+    include_templing!("src/gens/python/struct_impl.templing")
 }
 
 impl crate::Generator for Generator {
@@ -303,22 +105,6 @@ impl crate::Generator for Generator {
                 base_name,
                 variants,
             } => {
-                let file_name = format!("model/{}.py", base_name.snake_case(conv));
-                let mut writer = Writer::new();
-                writeln!(writer, "from enum import IntEnum").unwrap();
-                writeln!(writer).unwrap();
-                writeln!(writer, "class {}(IntEnum):", base_name.camel_case(conv)).unwrap();
-                writer.inc_ident();
-                for (index, variant) in variants.iter().enumerate() {
-                    writeln!(
-                        writer,
-                        "{} = {}",
-                        variant.name.shouty_snake_case(conv),
-                        index
-                    )
-                    .unwrap();
-                }
-                writer.dec_ident();
                 writeln!(
                     &mut self.model_init,
                     "from .{} import {}",
@@ -326,12 +112,12 @@ impl crate::Generator for Generator {
                     base_name.camel_case(conv),
                 )
                 .unwrap();
-                self.files.insert(file_name, writer.get());
+                self.files.insert(
+                    format!("model/{}.py", base_name.snake_case(conv)),
+                    include_templing!("src/gens/python/enum.templing"),
+                );
             }
             Schema::Struct(struc) => {
-                let file_name = format!("model/{}.py", struc.name.snake_case(conv));
-                let mut writer = Writer::new();
-                write_struct(&mut writer, struc, None).unwrap();
                 writeln!(
                     &mut self.model_init,
                     "from .{} import {}",
@@ -339,47 +125,16 @@ impl crate::Generator for Generator {
                     struc.name.camel_case(conv)
                 )
                 .unwrap();
-                self.files.insert(file_name, writer.get());
+                self.files.insert(
+                    format!("model/{}.py", struc.name.snake_case(conv)),
+                    include_templing!("src/gens/python/struct.templing"),
+                );
             }
             Schema::OneOf {
                 documentation: _,
                 base_name,
                 variants,
             } => {
-                let file_name = format!("model/{}.py", base_name.snake_case(conv));
-                let mut writer = Writer::new();
-                writeln!(&mut writer, "class {}:", base_name.camel_case(conv)).unwrap();
-                {
-                    writer.inc_ident();
-                    writeln!(&mut writer, "@staticmethod").unwrap();
-                    writeln!(&mut writer, "def read_from(stream):").unwrap();
-                    {
-                        writer.inc_ident();
-                        writeln!(&mut writer, "tag = stream.read_int()").unwrap();
-                        for variant in variants {
-                            writeln!(
-                                &mut writer,
-                                "if tag == {}.TAG:",
-                                variant.name.camel_case(conv)
-                            )
-                            .unwrap();
-                            writeln!(
-                                &mut writer,
-                                "    return {}.{}.read_from(stream)",
-                                base_name.camel_case(conv),
-                                variant.name.camel_case(conv),
-                            )
-                            .unwrap();
-                        }
-                        writeln!(&mut writer, "raise Exception(\"Unexpected tag value\")").unwrap();
-                        writer.dec_ident();
-                    }
-                    writer.dec_ident();
-                }
-                writeln!(&mut writer).unwrap();
-                for (tag, variant) in variants.iter().enumerate() {
-                    write_struct(&mut writer, variant, Some((base_name, tag))).unwrap();
-                }
                 writeln!(
                     &mut self.model_init,
                     "from .{} import {}",
@@ -387,7 +142,10 @@ impl crate::Generator for Generator {
                     base_name.camel_case(conv)
                 )
                 .unwrap();
-                self.files.insert(file_name, writer.get());
+                self.files.insert(
+                    format!("model/{}.py", base_name.snake_case(conv)),
+                    include_templing!("src/gens/python/oneof.templing"),
+                );
             }
             Schema::Bool
             | Schema::Int32
