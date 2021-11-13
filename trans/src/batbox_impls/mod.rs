256a80cca35223264f3fe5b1bd6eb590f8910dbb
kuviman <kuviman@gmail.com>
2021-11-13 06:43:47 +0400

    impl Trans for Collection

--- trans/src/batbox_impls/mod.rs
+++ trans/src/batbox_impls/mod.rs
@@ -79,3 +79,42 @@ impl<T: batbox::Float + Trans> Trans for batbox::RealImpl<T> {
         self.raw().write_to(writer, version)
     }
 }
+
+impl<T: Trans + batbox::HasId> Trans for batbox::Collection<T> {
+    fn create_schema(version: &Version) -> Schema {
+        Schema::Vec(Schema::of::<T>(version))
+    }
+    fn read_from(reader: &mut dyn std::io::Read, version: &Version) -> std::io::Result<Self> {
+        let len = usize::read_from(reader, version)?;
+        let mut result = batbox::Collection::new();
+        for _ in 0..len {
+            result.insert(T::read_from(reader, version)?);
+        }
+        Ok(result)
+    }
+    fn read_from_limited(
+        reader: &mut dyn std::io::Read,
+        limit: usize,
+        version: &Version,
+    ) -> std::io::Result<Self> {
+        let len = usize::read_from(reader, version)?;
+        if len > limit {
+            return Err(std::io::Error::new(
+                std::io::ErrorKind::Other,
+                format!("Length limit of {} exceeded, got {}", limit, len),
+            ));
+        }
+        let mut result = batbox::Collection::new();
+        for _ in 0..len {
+            result.insert(T::read_from(reader, version)?);
+        }
+        Ok(result)
+    }
+    fn write_to(&self, writer: &mut dyn std::io::Write, version: &Version) -> std::io::Result<()> {
+        self.len().write_to(writer, version)?;
+        for item in self {
+            item.write_to(writer, version)?;
+        }
+        Ok(())
+    }
+}
