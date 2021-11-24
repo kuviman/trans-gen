a63aebe5f2895066d8ef5a082bbd17402260e812
kuviman <kuviman@gmail.com>
2021-11-25 03:55:36 +0400

    C#: move all fns to methods

--- src/gens/csharp/mod.rs
+++ src/gens/csharp/mod.rs
@@ -17,218 +17,220 @@ impl Generator {
     pub fn main_namespace(&self) -> &str {
         &self.main_namespace
     }
-}
 
-fn new_var(var: &str, suffix: &str) -> String {
-    let var = match var.find('.') {
-        Some(index) => &var[index + 1..],
-        None => var,
-    };
-    let indexing_count = var.chars().filter(|&c| c == '[').count();
-    let var = match var.find('[') {
-        Some(index) => &var[..index],
-        None => var,
-    };
-    let mut var = var.to_owned();
-    for _ in 0..indexing_count {
-        var.push_str("Element");
-    }
-    var.push_str(suffix);
-    Name::new(var).mixed_case(conv)
-}
-
-fn is_null(var: &str, schema: &Schema) -> String {
-    if nullable(schema) {
-        format!("{} == null", var)
-    } else {
-        format!("!{}.HasValue", var)
-    }
-}
-
-fn option_unwrap(var: &str, schema: &Schema) -> String {
-    if nullable(schema) {
-        var.to_owned()
-    } else {
-        format!("{}.Value", var)
-    }
-}
-
-fn nullable(schema: &Schema) -> bool {
-    match schema {
-        Schema::Bool
-        | Schema::Int32
-        | Schema::Int64
-        | Schema::Float32
-        | Schema::Float64
-        | Schema::Enum { .. }
-        | Schema::Struct { .. } => false,
-        Schema::String
-        | Schema::Option(_)
-        | Schema::Vec(_)
-        | Schema::Map(_, _)
-        | Schema::OneOf { .. } => true,
-    }
-}
-
-pub fn type_name(schema: &Schema) -> String {
-    format!(
-        "{}{}",
-        type_name_prearray(schema),
-        type_name_postarray(schema),
-    )
-}
-
-fn type_name_prearray(schema: &Schema) -> String {
-    match schema {
-        Schema::Bool => "bool".to_owned(),
-        Schema::Int32 => "int".to_owned(),
-        Schema::Int64 => "long".to_owned(),
-        Schema::Float32 => "float".to_owned(),
-        Schema::Float64 => "double".to_owned(),
-        Schema::String => "string".to_owned(),
-        Schema::Struct { .. } | Schema::OneOf { .. } | Schema::Enum { .. } => name_path(schema),
-        Schema::Option(inner) => {
-            if nullable(inner) {
-                type_name(inner)
-            } else {
-                format!("{}?", type_name(inner))
-            }
+    fn new_var(&self, var: &str, suffix: &str) -> String {
+        let var = match var.find('.') {
+            Some(index) => &var[index + 1..],
+            None => var,
+        };
+        let indexing_count = var.chars().filter(|&c| c == '[').count();
+        let var = match var.find('[') {
+            Some(index) => &var[..index],
+            None => var,
+        };
+        let mut var = var.to_owned();
+        for _ in 0..indexing_count {
+            var.push_str("Element");
         }
-        Schema::Vec(inner) => type_name_prearray(inner),
-        Schema::Map(key, value) => format!(
-            "System.Collections.Generic.IDictionary<{}, {}>",
-            type_name(key),
-            type_name(value)
-        ),
+        var.push_str(suffix);
+        Name::new(var).mixed_case(conv)
     }
-}
 
-fn type_name_postarray(schema: &Schema) -> String {
-    match schema {
-        Schema::Vec(inner) => format!("[]{}", type_name_postarray(inner)),
-        _ => String::new(),
+    fn is_null(&self, var: &str, schema: &Schema) -> String {
+        if self.nullable(schema) {
+            format!("{} == null", var)
+        } else {
+            format!("!{}.HasValue", var)
+        }
     }
-}
 
-pub fn default_value(schema: &Schema) -> String {
-    match schema {
-        Schema::Bool => "false".to_owned(),
-        Schema::Int32 | Schema::Int64 => "0".to_owned(),
-        Schema::Float32 | Schema::Float64 => "0.0".to_owned(),
-        Schema::Map(key, value) => format!(
-            "new System.Collections.Generic.Dictionary<{}, {}>()",
-            type_name(key),
-            type_name(value),
-        ),
-        Schema::Vec(inner) => format!(
-            "new {}[0]{}",
-            type_name_prearray(inner),
-            type_name_postarray(inner),
-        ),
-        Schema::Option(..) => "null".to_owned(),
-        Schema::String => unimplemented!("No default string"),
-        Schema::Struct { definition, .. } => {
-            let mut result = format!("new {}(", type_name(schema));
-            for (index, field) in definition.fields.iter().enumerate() {
-                if index != 0 {
-                    result.push_str(", ");
+    fn option_unwrap(&self, var: &str, schema: &Schema) -> String {
+        if self.nullable(schema) {
+            var.to_owned()
+        } else {
+            format!("{}.Value", var)
+        }
+    }
+
+    fn nullable(&self, schema: &Schema) -> bool {
+        match schema {
+            Schema::Bool
+            | Schema::Int32
+            | Schema::Int64
+            | Schema::Float32
+            | Schema::Float64
+            | Schema::Enum { .. }
+            | Schema::Struct { .. } => false,
+            Schema::String
+            | Schema::Option(_)
+            | Schema::Vec(_)
+            | Schema::Map(_, _)
+            | Schema::OneOf { .. } => true,
+        }
+    }
+
+    pub fn type_name(&self, schema: &Schema) -> String {
+        format!(
+            "{}{}",
+            self.type_name_prearray(schema),
+            self.type_name_postarray(schema),
+        )
+    }
+
+    fn type_name_prearray(&self, schema: &Schema) -> String {
+        match schema {
+            Schema::Bool => "bool".to_owned(),
+            Schema::Int32 => "int".to_owned(),
+            Schema::Int64 => "long".to_owned(),
+            Schema::Float32 => "float".to_owned(),
+            Schema::Float64 => "double".to_owned(),
+            Schema::String => "string".to_owned(),
+            Schema::Struct { .. } | Schema::OneOf { .. } | Schema::Enum { .. } => {
+                self.name_path(schema)
+            }
+            Schema::Option(inner) => {
+                if self.nullable(inner) {
+                    self.type_name(inner)
+                } else {
+                    format!("{}?", self.type_name(inner))
                 }
-                result.push_str(&default_value(&field.schema));
             }
-            result.push(')');
-            result
+            Schema::Vec(inner) => self.type_name_prearray(inner),
+            Schema::Map(key, value) => format!(
+                "System.Collections.Generic.IDictionary<{}, {}>",
+                self.type_name(key),
+                self.type_name(value)
+            ),
         }
-        Schema::Enum { .. } => unimplemented!("Can't determine default enum variant"),
-        Schema::OneOf { .. } => unimplemented!("Can't determine default OneOf variant"),
     }
-}
 
-fn doc_comment(documentation: &Documentation) -> String {
-    let mut result = String::new();
-    result.push_str("/// <summary>\n");
-    for line in documentation.get("en").unwrap().lines() {
-        result.push_str("/// ");
-        result.push_str(line);
-        result.push('\n');
-    }
-    result.push_str("/// </summary>\n");
-    result.trim().to_owned()
-}
-
-fn doc_read_from(name: &str) -> String {
-    format!("/// <summary> Read {} from reader </summary>", name)
-}
-
-fn doc_write_to(name: &str) -> String {
-    format!("/// <summary> Write {} to writer </summary>", name)
-}
-
-fn doc_to_string(name: &str) -> String {
-    format!(
-        "/// <summary> Get string representation of {} </summary>",
-        name,
-    )
-}
-
-fn read_var(var: &str, schema: &Schema) -> String {
-    include_templing!("src/gens/csharp/read_var.templing")
-}
-
-fn write_var(var: &str, schema: &Schema) -> String {
-    include_templing!("src/gens/csharp/write_var.templing")
-}
-
-fn var_to_string(var: &str, schema: &Schema) -> String {
-    include_templing!("src/gens/csharp/var_to_string.templing")
-}
-
-fn struct_impl(definition: &Struct, base: Option<(&Name, usize)>) -> String {
-    include_templing!("src/gens/csharp/struct_impl.templing")
-}
-
-fn namespace_path(namespace: &Namespace) -> String {
-    namespace
-        .parts
-        .iter()
-        .map(|name| name.camel_case(conv))
-        .collect::<Vec<_>>()
-        .join(".")
-}
-
-fn namespace_path_suffix(namespace: &Namespace) -> String {
-    let namespace_path = namespace_path(namespace);
-    if namespace_path.is_empty() {
-        namespace_path
-    } else {
-        format!(".{}", namespace_path)
-    }
-}
-
-fn name_path(schema: &Schema) -> String {
-    match schema {
-        Schema::Enum {
-            namespace,
-            base_name: name,
-            ..
+    fn type_name_postarray(&self, schema: &Schema) -> String {
+        match schema {
+            Schema::Vec(inner) => format!("[]{}", self.type_name_postarray(inner)),
+            _ => String::new(),
         }
-        | Schema::Struct {
-            namespace,
-            definition: Struct { name, .. },
-            ..
-        }
-        | Schema::OneOf {
-            namespace,
-            base_name: name,
-            ..
-        } => {
-            let namespace_path = namespace_path(namespace);
-            if namespace_path.is_empty() {
-                name.camel_case(conv)
-            } else {
-                format!("{}.{}", namespace_path, name.camel_case(conv))
+    }
+
+    pub fn default_value(&self, schema: &Schema) -> String {
+        match schema {
+            Schema::Bool => "false".to_owned(),
+            Schema::Int32 | Schema::Int64 => "0".to_owned(),
+            Schema::Float32 | Schema::Float64 => "0.0".to_owned(),
+            Schema::Map(key, value) => format!(
+                "new System.Collections.Generic.Dictionary<{}, {}>()",
+                self.type_name(key),
+                self.type_name(value),
+            ),
+            Schema::Vec(inner) => format!(
+                "new {}[0]{}",
+                self.type_name_prearray(inner),
+                self.type_name_postarray(inner),
+            ),
+            Schema::Option(..) => "null".to_owned(),
+            Schema::String => unimplemented!("No default string"),
+            Schema::Struct { definition, .. } => {
+                let mut result = format!("new {}(", self.type_name(schema));
+                for (index, field) in definition.fields.iter().enumerate() {
+                    if index != 0 {
+                        result.push_str(", ");
+                    }
+                    result.push_str(&self.default_value(&field.schema));
+                }
+                result.push(')');
+                result
             }
+            Schema::Enum { .. } => unimplemented!("Can't determine default enum variant"),
+            Schema::OneOf { .. } => unimplemented!("Can't determine default OneOf variant"),
         }
-        _ => unreachable!(),
+    }
+
+    fn doc_comment(&self, documentation: &Documentation) -> String {
+        let mut result = String::new();
+        result.push_str("/// <summary>\n");
+        for line in documentation.get("en").unwrap().lines() {
+            result.push_str("/// ");
+            result.push_str(line);
+            result.push('\n');
+        }
+        result.push_str("/// </summary>\n");
+        result.trim().to_owned()
+    }
+
+    fn doc_read_from(&self, name: &str) -> String {
+        format!("/// <summary> Read {} from reader </summary>", name)
+    }
+
+    fn doc_write_to(&self, name: &str) -> String {
+        format!("/// <summary> Write {} to writer </summary>", name)
+    }
+
+    fn doc_to_string(&self, name: &str) -> String {
+        format!(
+            "/// <summary> Get string representation of {} </summary>",
+            name,
+        )
+    }
+
+    fn read_var(&self, var: &str, schema: &Schema) -> String {
+        include_templing!("src/gens/csharp/read_var.templing")
+    }
+
+    fn write_var(&self, var: &str, schema: &Schema) -> String {
+        include_templing!("src/gens/csharp/write_var.templing")
+    }
+
+    fn var_to_string(&self, var: &str, schema: &Schema) -> String {
+        include_templing!("src/gens/csharp/var_to_string.templing")
+    }
+
+    fn struct_impl(&self, definition: &Struct, base: Option<(&Name, usize)>) -> String {
+        include_templing!("src/gens/csharp/struct_impl.templing")
+    }
+
+    fn namespace_path(&self, namespace: &Namespace) -> String {
+        namespace
+            .parts
+            .iter()
+            .map(|name| name.camel_case(conv))
+            .collect::<Vec<_>>()
+            .join(".")
+    }
+
+    fn namespace_path_suffix(&self, namespace: &Namespace) -> String {
+        let namespace_path = self.namespace_path(namespace);
+        if namespace_path.is_empty() {
+            namespace_path
+        } else {
+            format!(".{}", namespace_path)
+        }
+    }
+
+    fn name_path(&self, schema: &Schema) -> String {
+        match schema {
+            Schema::Enum {
+                namespace,
+                base_name: name,
+                ..
+            }
+            | Schema::Struct {
+                namespace,
+                definition: Struct { name, .. },
+                ..
+            }
+            | Schema::OneOf {
+                namespace,
+                base_name: name,
+                ..
+            } => {
+                let namespace_path = self.namespace_path(namespace);
+                if namespace_path.is_empty() {
+                    name.camel_case(conv)
+                } else {
+                    format!("{}.{}", namespace_path, name.camel_case(conv))
+                }
+            }
+            _ => unreachable!(),
+        }
     }
 }
 
@@ -262,7 +264,7 @@ impl crate::Generator for Generator {
                 variants,
             } => {
                 self.files.insert(
-                    format!("{}.cs", name_path(schema).replace('.', "/")),
+                    format!("{}.cs", self.name_path(schema).replace('.', "/")),
                     include_templing!("src/gens/csharp/enum.templing"),
                 );
             }
@@ -271,7 +273,7 @@ impl crate::Generator for Generator {
                 definition,
             } => {
                 self.files.insert(
-                    format!("{}.cs", name_path(schema).replace('.', "/")),
+                    format!("{}.cs", self.name_path(schema).replace('.', "/")),
                     include_templing!("src/gens/csharp/struct.templing"),
                 );
             }
@@ -282,7 +284,7 @@ impl crate::Generator for Generator {
                 variants,
             } => {
                 self.files.insert(
-                    format!("{}.cs", name_path(schema).replace('.', "/")),
+                    format!("{}.cs", self.name_path(schema).replace('.', "/")),
                     include_templing!("src/gens/csharp/oneof.templing"),
                 );
             }
