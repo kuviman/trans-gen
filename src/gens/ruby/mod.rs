2d5682110ec10e116096b77f665f6f171e40c156
kuviman <kuviman@gmail.com>
2021-11-25 07:29:36 +0400

    Ruby default values

--- src/gens/ruby/mod.rs
+++ src/gens/ruby/mod.rs
@@ -108,6 +108,32 @@ pub fn file_name(schema: &Schema) -> String {
         .join("/")
 }
 
+pub fn default_value(schema: &Schema) -> String {
+    match schema {
+        Schema::Bool => "false".to_owned(),
+        Schema::Int32 | Schema::Int64 => "0".to_owned(),
+        Schema::Float32 | Schema::Float64 => "0.0".to_owned(),
+        Schema::Map(..) => "Hash.new".to_owned(),
+        Schema::Vec(..) => "[]".to_owned(),
+        Schema::Option(..) => "nil".to_owned(),
+        Schema::String => unimplemented!("No default string"),
+        Schema::Struct { definition, .. } => {
+            let mut result = type_name(schema);
+            result.push_str(".new(");
+            for (index, field) in definition.fields.iter().enumerate() {
+                if index != 0 {
+                    result.push_str(", ");
+                }
+                result.push_str(&default_value(&field.schema));
+            }
+            result.push(')');
+            result
+        }
+        Schema::Enum { .. } => unimplemented!("Can't determine default enum variant"),
+        Schema::OneOf { .. } => unimplemented!("Can't determine default OneOf variant"),
+    }
+}
+
 pub fn type_name(schema: &Schema) -> String {
     schema
         .namespace()
